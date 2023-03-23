d9b7a374d80cc6a04905e6d0e883525fc11f9443
miralouisepaquin <90649621+miralouisepaquin@users.noreply.github.com>
2023-03-23 09:48:48 -0400

    Ajout de l'adresse Broker
    Ajout de l'adresse Broker associer au user et ajout deleteAllUsers

--- app/routes/users.routes.js
+++ app/routes/users.routes.js
@@ -1,16 +1,18 @@
 module.exports = app => {
-    const users = require("../controllers/users.controller.js");
+    const usersController = require("../controllers/users.controller.js");
     var router = require("express").Router();
     // Create a new user
-    router.post("/", users.create);
+    router.post("/createUser", usersController.create);
     // Retrieve all users
-    router.get("/", users.findAll);
+    router.get("/findAllUsers", usersController.findAll);
     // Retrieve a single user with id
-    router.get("/:id", users.findOne);
+    router.get("/findUser/:id", usersController.findOne);
     // Update a user with id
-    router.put("/:id", users.update);
+    router.put("/updateUser/:id", usersController.update);
     // Delete a user with id
-    router.delete("/:id", users.delete);
+    router.delete("/deleteUser/:id", usersController.delete);
+	// Delete all logs
+    router.delete("/deleteAllUsers", usersController.deleteAll);
 	
 	app.use('/api/users', router);
   };
